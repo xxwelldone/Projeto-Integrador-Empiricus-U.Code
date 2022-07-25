20783758e79f05734faee757fad2ce3d41d49fcf
Wesley Menezes <wesleymenezesdnovaes@gmail.com>
2022-07-25 19:26:50 -0300

    Abstract class

---
+++ Projeto Banco/src/Entities/Conta.java
@@ -0,0 +1,52 @@
+package Entities;
+
+public abstract class Conta {
+	private int numero;
+	private String cpf ;
+	private double saldo;
+	private boolean ativo;
+	
+	// Getters and Setters 
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	/*Saldo does not have a setter for security reasons*/
+	
+	public boolean isAtivo() {
+		return ativo;
+	}
+	public void setAtivo(boolean ativo) {
+		this.ativo = ativo;
+	}
+	
+	// Constructor 
+	public Conta(int numero, String cpf) {
+		
+		this.numero = numero;
+		this.cpf = cpf;
+	}
+	// Methods Withdraw and Credit 
+	
+	public void debito (double valor) {
+		if (valor<saldo) {
+			saldo -= valor;
+		} else {System.out.println("O valor informado é maior que saldo disponivel");
+		}
+	}
+	public void credito (double valor) {
+		saldo = saldo + valor;
+	}
+	
+}
