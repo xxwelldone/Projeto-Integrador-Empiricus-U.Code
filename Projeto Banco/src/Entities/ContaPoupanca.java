cb649abefc92418b96ec565550676893250c3644
Carla Vitória <vitoriacarlafs99@gmail.com>
2022-07-27 02:06:34 -0300

    Adicionado a classe Conta Poupança

---
+++ Projeto Banco/src/Entities/ContaPoupanca.java
@@ -0,0 +1,32 @@
+package Entities;
+
+import java.time.LocalDate;
+
+public class ContaPoupanca extends Conta { public ContaPoupanca(int numero, String cpf, int diaAniversarioPoupanca) {
+		super(numero, cpf);
+		this.diaAniversarioPoupanca = diaAniversarioPoupanca;
+	}
+
+	private int diaAniversarioPoupanca;
+
+	public int getDiaAniversarioPoupanca() {
+		return diaAniversarioPoupanca;
+	}
+
+	public void setDiaAniversarioPoupanca(int diaAniversarioPoupanca) {
+		this.diaAniversarioPoupanca = diaAniversarioPoupanca;
+	}
+
+	public void correcao (int dia) {
+		
+		if(diaAniversarioPoupanca == dia) {
+			double correcao = getSaldo()* 0.05;
+			credito(correcao);
+		}
+		
+
+	}
+
+	}
+	
+
